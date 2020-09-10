52043b60fd1c92756907ff655e50b99a30b1d16b
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 18:54:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speaker_list)
+    badges = []
+    speaker_list.each do |attendees|
+      badges << "Hello, my name is #{attendees}."
+    end
+    badges
+end
+
+def assign_rooms(speaker_list)
+  room_assignments = []
+  speaker_list.each_with_index do |attendee, index|
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
+end
+    
+def printer
+  batch_badge_creator.each { |text| } 
+  puts text
+  assign_rooms.each { |message| }
+  puts message
+end
